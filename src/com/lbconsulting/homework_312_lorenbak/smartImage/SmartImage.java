9e9a6311af2c709f70d0ea2b0644c31537c05272
LorenBaker <LorenABaker@comcast.net>
2014-04-23 15:49:58 -0700

    Added v7 appcompat

--- src/com/lbconsulting/homework_312_lorenbak/smartImage/SmartImage.java
+++
@@ -1,9 +0,0 @@
-package com.lbconsulting.homework_312_lorenbak.smartImage;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-
-public interface SmartImage {
-
-	public Bitmap getBitmap(Context context);
-}
